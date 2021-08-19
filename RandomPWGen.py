3f9d8ae746ffd7c00b65b87c97636850aa05e5d4
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2021-08-19 12:17:45 +0200

    Update RandomPWGen.py

--- RandomPWGen.py
+++ RandomPWGen.py
@@ -2,7 +2,7 @@ import random
 import string
 l = input("How long should your password be? My Answer: ")
 n = int(l)
-if n > 5000:
+if n > 8192:
 	print("Well... Thats a little too big for a password! Try a lower number!")
 else:  
     res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = n))
